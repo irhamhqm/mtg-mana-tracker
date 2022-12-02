cc206f31cb626c45292ef9e3151d841a35da6d44
irhamhkm <irhamhqm@gmail.com>
2022-12-03 03:16:49 +0700

    revamp mana item stylings

--- components/Mana.tsx
+++ components/Mana.tsx
@@ -13,9 +13,9 @@ export default function Mana(props: ManaProps) {
   return (
     <div className="flex flex-col align-center items-center text-center">
       <Image src={`/${image}.png`} alt="mana" width={150} height={150} />
-      <div>
+      <div className="mt-4">
         <button className="p-3 font-bold text-lg" disabled={!counter} onClick={handleSub}>-</button>
-        <input className="p-3 w-12 h-12 font-bold text-lg" value={counter} onChange={(e) => handleSet(parseInt(e.target.value, 10))}/>
+        <input className="bg-gray-200 p-3 w-12 h-12 font-bold text-lg text-center" value={counter} onChange={(e) => handleSet(parseInt(e.target.value, 10))}/>
         <button className="p-3 font-bold text-lg" onClick={handleAdd}>+</button>
       </div>
     </div>
